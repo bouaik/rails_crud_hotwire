34abc14c0739ab2d32da1ef0a4ab1348898089b2
bouaik <lhoussaine.bouaik@um6p.ma>
2024-02-27 23:55:13 +0100

    little tweaks

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,9 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { get } from "@rails/request.js"
 
 export default class extends Controller {
-  connect() {
-    console.log("hello controller")
-  }
+  connect() {}
 
   async update(e) {
     const res = await get(`/apex_update/${e.target.value}`)
